f5e7346f42654a7f5c62f016bd66f04b3ead7550
emmanuel-dev <emmanuel.rodriguez@timbox.com.mx>
2023-01-27 17:30:53 -0600

    MOD: correccion de error por formArray

--- src/app/views/work-area/create-xml/xml-concepts/xml-concepts.component.ts
+++ src/app/views/work-area/create-xml/xml-concepts/xml-concepts.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, FormArray, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder,  Validators, FormArray } from '@angular/forms';
 import { XmlConceptService } from './../../../../services/create-xml/xml-concept.service';
 import { SweetAlertsService } from 'src/app/services/sweet-alert.service';
 
@@ -16,27 +16,13 @@ export class XmlConceptsComponent implements OnInit {
   itemsAccordion: any = []
   formAddConcept: FormGroup = new FormGroup({});
 
-  formName!: FormGroup;
+  mainForm!: FormGroup;
 
   constructor(private _service: XmlConceptService, private swal: SweetAlertsService,
     private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
     this.formAddConcept = this.formBuilder.group(this._service.getDataValidateConcept());
-
-    this.formName = this.formBuilder.group({
-      productKey: ['', [Validators.required]],// ClaveProdServ
-      quantity: ['', Validators.required],// Cantidad
-      unitKey: ['', Validators.required],// ClaveUnidad
-      description: ['', [Validators.required]],// Descripcion
-      unitValue: ['', [Validators.required]],// ValorUnitario
-      amount: ['', [Validators.required]], //Importe
-      taxObject: ['', Validators.required],// ObjetoImp
-      idNumber: [''], //NoIdentificacion
-      unit: [''],// Unidad
-      discount: [''], //Descuento
-      items: this.formBuilder.array([this.createItem()])
-    });
 
     this.getConcepts();
 
@@ -58,23 +44,6 @@ export class XmlConceptsComponent implements OnInit {
     this.itemsAccordion.push(concept);
   }
 
-  getControls() {
-    return  (this.formName.get('items') as FormArray).controls
-  }
 
-  private createItem(): FormGroup {
-    return this.formBuilder.group({
-      productKey: '',
-      quantity: '',
-      unitKey: '',
-      description: '',
-      unitValue: '',
-      amount: '',
-      taxObject: '',
-      idNumber: '',
-      unit: '',
-      discount: ''
-    });
-  }
 
 }
