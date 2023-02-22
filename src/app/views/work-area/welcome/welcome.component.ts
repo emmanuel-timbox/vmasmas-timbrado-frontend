657a1a1eb44b07a88bc1edd7a89a5e4710eae955
emmanuel-dev <emmanuel.rodriguez@timbox.com.mx>
2023-02-22 15:01:07 -0600

    MOD: mostrar el listado de los xml timbrados

--- src/app/views/work-area/welcome/welcome.component.ts
+++ src/app/views/work-area/welcome/welcome.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
+import { WelcomeService } from './../../../services/welcome.service';
 
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
   styleUrls: ['./welcome.component.scss']
 })
+
 export class WelcomeComponent implements OnInit {
 
-  constructor() { }
+  dtOptions: DataTables.Settings = {};
+  dtTrigger: Subject<any> = new Subject<any>();
+  dataXml!: any;
+  typeVaoucher: any = { 'I': 'Ingreso', 'E': 'Egreso', 'N': 'Nomina' }
+
+  constructor(private _service: WelcomeService) { }
 
   ngOnInit(): void {
+    this.dtOptions = {
+      lengthMenu: [5, 10, 25, 50, 100],
+      pageLength: 25,
+      language: { url: 'https://cdn.datatables.net/plug-ins/1.10.15/i18n/Spanish.json' },
+      destroy: true
+    };
+
+    this.getDataXml();
+  }
+
+  getDataXml(): void {
+    this._service.getXmlsData().subscribe({
+      next: response => {
+        const result: any = JSON.parse(JSON.stringify(response));
+        this.dataXml = result.data;
+        this.dtTrigger.next(null);
+      },
+      error: error => { console.log(error); }
+    });
   }
 
 }
