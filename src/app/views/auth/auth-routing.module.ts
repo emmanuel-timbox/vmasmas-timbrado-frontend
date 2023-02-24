c38c9ce13eb0d7dc1be96ceb21fc47068d0a1a39
emmanuel-dev <emmanuel.rodriguez@timbox.com.mx>
2023-02-24 13:14:31 -0600

    ADD: creacion de componentes para la autentificacion

--- src/app/views/auth/auth-routing.module.ts
+++ src/app/views/auth/auth-routing.module.ts
@@ -2,9 +2,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
+import { RegistrerComponent } from './registrer/registrer.component';
+import { RecoverPasswordComponent } from './recover-password/recover-password.component';
+import { UpdatePasswordComponent } from './update-password/update-password.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
+  { path: 'registrer', component: RegistrerComponent },
+  { path: 'recover-password', component: RecoverPasswordComponent },
+  { path: 'update-password', component: UpdatePasswordComponent}
 ];
 
 @NgModule({
--- src/app/views/auth/auth.module.ts
+++ src/app/views/auth/auth.module.ts
@@ -1,14 +1,19 @@
 
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RecoverPasswordComponent } from './recover-password/recover-password.component';
+import { UpdatePasswordComponent } from './update-password/update-password.component';
+import { RegistrerComponent } from './registrer/registrer.component';
 import { LoginComponent } from './login/login.component';
 import { RouterModule } from '@angular/router';
 import { FragmentsTemplateModule } from '../fragments-template/fragments-template.module';
 
 @NgModule({
   declarations: [
-
-    LoginComponent
+    RecoverPasswordComponent,
+    UpdatePasswordComponent,
+    LoginComponent,
+    RegistrerComponent
   ],
   imports: [
     CommonModule,
@@ -16,7 +21,10 @@ import { FragmentsTemplateModule } from '../fragments-template/fragments-templat
     FragmentsTemplateModule
   ],
   exports: [
+    RecoverPasswordComponent,
+    UpdatePasswordComponent,
     LoginComponent,
+    RegistrerComponent
   ],
 })
 export class AuthModule { }
