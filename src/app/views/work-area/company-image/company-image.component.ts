07f2318d1d6705c31c448018db8fd913a1059c57
emmanuel-dev <emmanuel.rodriguez@timbox.com.mx>
2023-03-02 19:01:30 -0600

    MOD: botones customisado para cargar los images de logo y marca de agua

--- src/app/views/work-area/company-image/company-image.component.ts
+++ src/app/views/work-area/company-image/company-image.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-company-image',
@@ -6,6 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./company-image.component.scss']
 })
 
-export class CompanyImageComponent {
+export class CompanyImageComponent implements OnInit {
+  @ViewChild('fileUpload', { static: false }) pdfView!: ElementRef;
+  @ViewChild('fileWaterMark', { static: false }) fileWaterMark!: ElementRef;
+
+  haveImage: boolean = false;
+  logoImage!: File;
+  logoUrl!: string;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  selectInput() {
+    alert('Hola mundo')
+  }
+
+  importFile(event: Event) {
+    const render = new FileReader();
+    const elementTarge: FileList | null = (event.target as HTMLInputElement).files;
+
+    this.logoImage = elementTarge![0];
+
+    render.readAsDataURL(this.logoImage);
+    render.onload = (event: any) => {
+      this.logoUrl = event.target.result;
+    }
+  }
 
 }
